f6bf2113d6e8f33ec3d676fda5cb4d8f9d5f19f8
anam88 <amasroor88@gmail.com>
2020-12-18 17:06:26 -0600

    added plugin

--- src/test/java/com/myapp/runners/TestRunner.java
+++ src/test/java/com/myapp/runners/TestRunner.java
@@ -5,9 +5,13 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {
+                "json:target/cucumber.json"
+        },
         features = "src/test/resources/features",
         glue = "com/myapp/step_definitions",
         dryRun = false
+
 )
 
 public class TestRunner {
